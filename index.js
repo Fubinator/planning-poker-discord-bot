96db5b324d851cc354307fa784e2365d5b5e4a26
milan090 <milanmuhammed1@gmail.com>
2020-10-01 12:49:54 +0530

    Implemented switch and removes most if statements

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
-
 const { Client, Collection } = require("discord.js");
 
 const Poker = require("./poker");
-
 const client = new Client();
-
 const games = new Collection();
 
 client.on("ready", () => {
@@ -13,76 +10,85 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.author.username === "planning-poker-bot") return;
-
-  if (message.content === "!start") {
-    if (games.has(message.channel.id)) {
-      message.channel.send("Game is already in progress in this channel!");
-      return;
-    }
-    games.set(message.channel.id, true);
-    message.channel.send("Welcome to planning poker");
-    message.channel.send("Please start your first round with !play [QUESTION]");
-    message.channel.send(
-      "For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
-    );
-    message.channel.send("You can stop playing by typing !end");
-
-    return;
-  }
-
-  if (message.content.startsWith("!play")) {
-    if (Poker.isQuestionRunning) return;
-
-    const question = message.content.split(" ").splice(1).join(" ");
-
-    message.channel.send(`First question: ${question}`);
-    message.channel.send("Please provide your guesses");
-
-    Poker.playQuestion(question);
-
-    setTimeout(() => {
-      message.channel.send("Time's up!");
-      message.channel.send(
-        "Please enter the number of storypoints with !storypoints [AMOUNT] when you finished discussing"
-      );
-      for (const answer of Poker.currentAnswers) {
-        message.channel.send(`${answer.user}: ${answer.points}`);
-      }
-    }, 30 * 1000);
-
-    return;
-  }
-
-  if (message.content.startsWith("!storypoints")) {
-    if (!Poker.isQuestionRunning) return;
-
-    const storypoints = message.content.split(" ")[1];
-
-    message.channel.send(
-      `Added ${storypoints} to your question ${Poker.currentQuestion}`
-    );
-
-    Poker.finishQuestion(storypoints);
-  }
-
-  if (message.content === "!end") {
-    message.channel.send("Planning Poker finished");
-    message.channel.send("Here is an overview of your game:");
-    if (games.has(message.channel.id)) games.delete(message.channel.id);
-
-    for (const question of Poker.questions)
-      message.channel.send(
-        `Question: ${question.question} Story Points: ${question.storypoints}`
-      );
-
-    Poker.finishGame();
-
-    return;
-  }
+  if (message.author.bot) return;
 
   if (message.channel.type === "dm" && Poker.isQuestionRunning) {
     Poker.addAnswer(message.author.username, message.content);
+  }
+
+  const args = message.content.split(" ");
+  const fullCommand = args.shift(); // Command now holds the first word and args holds rest of them
+  if (!fullCommand.startsWith(process.env.PREFIX)) return;
+  const command = fullCommand.slice(1); // Removes the prefix
+
+  switch (command) {
+    case "start":
+      if (games.has(message.channel.id)) {
+        message.channel.send("Game is already in progress in this channel!");
+        return;
+      }
+      games.set(message.channel.id, true);
+      message.channel.send("Welcome to planning poker");
+      message.channel.send(
+        "Please start your first round with !play [QUESTION]"
+      );
+      message.channel.send(
+        "For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
+      );
+      message.channel.send("You can stop playing by typing !end");
+
+      return;
+
+    case "play":
+      if (Poker.isQuestionRunning) return;
+
+      const question = message.content.split(" ").splice(1).join(" ");
+  
+      message.channel.send(`First question: ${question}`);
+      message.channel.send("Please provide your guesses");
+  
+      Poker.playQuestion(question);
+  
+      setTimeout(() => {
+        message.channel.send("Time's up!");
+        message.channel.send(
+          "Please enter the number of storypoints with !storypoints [AMOUNT] when you finished discussing"
+        );
+        for (const answer of Poker.currentAnswers) {
+          message.channel.send(`${answer.user}: ${answer.points}`);
+        }
+      }, 30 * 1000);
+  
+      return;
+
+    case "storypoints":
+      if (!Poker.isQuestionRunning) return;
+
+      const storypoints = message.content.split(" ")[1];
+  
+      message.channel.send(
+        `Added ${storypoints} to your question ${Poker.currentQuestion}`
+      );
+  
+      Poker.finishQuestion(storypoints);
+      break;
+    
+    case "end":
+      message.channel.send("Planning Poker finished");
+      message.channel.send("Here is an overview of your game:");
+      if (games.has(message.channel.id)) games.delete(message.channel.id);
+  
+      for (const question of Poker.questions)
+        message.channel.send(
+          `Question: ${question.question} Story Points: ${question.storypoints}`
+        );
+  
+      Poker.finishGame();
+  
+      return;
+
+    default:
+      break;
   }
 });
 
