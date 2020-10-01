2739c5ae1cae5899727354d64e59af0217df9ef7
milan090 <milanmuhammed1@gmail.com>
2020-10-01 12:58:19 +0530

    fixed linting issues

--- index.js
+++ index.js
@@ -22,73 +22,77 @@ client.on("message", (message) => {
   const command = fullCommand.slice(1); // Removes the prefix
 
   switch (command) {
-    case "start":
-      if (games.has(message.channel.id)) {
-        message.channel.send("Game is already in progress in this channel!");
-        return;
+  case "start": {
+    if (games.has(message.channel.id)) {
+      message.channel.send("Game is already in progress in this channel!");
+      return;
+    }
+    games.set(message.channel.id, true);
+    message.channel.send("Welcome to planning poker");
+    message.channel.send(
+      "Please start your first round with !play [QUESTION]"
+    );
+    message.channel.send(
+      "For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
+    );
+    message.channel.send("You can stop playing by typing !end");
+
+    return;
+  }
+
+  case "play": {
+    if (Poker.isQuestionRunning) return;
+
+    const question = message.content.split(" ").splice(1).join(" ");
+
+    message.channel.send(`First question: ${question}`);
+    message.channel.send("Please provide your guesses");
+
+    Poker.playQuestion(question);
+
+    setTimeout(() => {
+      message.channel.send("Time's up!");
+      message.channel.send(
+        "Please enter the number of storypoints with !storypoints [AMOUNT] when you finished discussing"
+      );
+      for (const answer of Poker.currentAnswers) {
+        message.channel.send(`${answer.user}: ${answer.points}`);
       }
-      games.set(message.channel.id, true);
-      message.channel.send("Welcome to planning poker");
+    }, 30 * 1000);
+
+    return;
+  }
+
+  case "storypoints": {
+    if (!Poker.isQuestionRunning) return;
+
+    const storypoints = message.content.split(" ")[1];
+
+    message.channel.send(
+      `Added ${storypoints} to your question ${Poker.currentQuestion}`
+    );
+
+    Poker.finishQuestion(storypoints);
+    break;
+  }
+
+  case "end": {
+    message.channel.send("Planning Poker finished");
+    message.channel.send("Here is an overview of your game:");
+    if (games.has(message.channel.id)) games.delete(message.channel.id);
+
+    for (const question of Poker.questions)
       message.channel.send(
-        "Please start your first round with !play [QUESTION]"
+        `Question: ${question.question} Story Points: ${question.storypoints}`
       );
-      message.channel.send(
-        "For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
-      );
-      message.channel.send("You can stop playing by typing !end");
 
-      return;
+    Poker.finishGame();
 
-    case "play":
-      if (Poker.isQuestionRunning) return;
+    return;
+  }
 
-      const question = message.content.split(" ").splice(1).join(" ");
-  
-      message.channel.send(`First question: ${question}`);
-      message.channel.send("Please provide your guesses");
-  
-      Poker.playQuestion(question);
-  
-      setTimeout(() => {
-        message.channel.send("Time's up!");
-        message.channel.send(
-          "Please enter the number of storypoints with !storypoints [AMOUNT] when you finished discussing"
-        );
-        for (const answer of Poker.currentAnswers) {
-          message.channel.send(`${answer.user}: ${answer.points}`);
-        }
-      }, 30 * 1000);
-  
-      return;
-
-    case "storypoints":
-      if (!Poker.isQuestionRunning) return;
-
-      const storypoints = message.content.split(" ")[1];
-  
-      message.channel.send(
-        `Added ${storypoints} to your question ${Poker.currentQuestion}`
-      );
-  
-      Poker.finishQuestion(storypoints);
-      break;
-    
-    case "end":
-      message.channel.send("Planning Poker finished");
-      message.channel.send("Here is an overview of your game:");
-      if (games.has(message.channel.id)) games.delete(message.channel.id);
-  
-      for (const question of Poker.questions)
-        message.channel.send(
-          `Question: ${question.question} Story Points: ${question.storypoints}`
-        );
-  
-      Poker.finishGame();
-  
-      return;
-
-    default:
-      break;
+  default:
+    break;
   }
 });
 
