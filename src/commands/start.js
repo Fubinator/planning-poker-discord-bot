f16812bf3a073145d1ad980566c257ce252c6e5b
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2020-10-02 14:38:32 +0530

    added ascii art

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,21 +1,40 @@
+const ascii1=`
+\`\`\`
+_____ _             _         
+|  _  | |___ ___ ___|_|___ ___ 
+|   __| | .'|   |   | |   | . |
+|__|  |_|__,|_|_|_|_|_|_|_|_  |
+                          |___|
+\`\`\`
+`
+const ascii2 = `
+\`\`\`
+_____     _              _____     _   
+|  _  |___| |_ ___ ___   | __  |___| |_ 
+|   __| . | '_| -_|  _|  | __ -| . |  _|
+|__|  |___|_,_|___|_|    |_____|___|_|                           
+\`\`\`
+`;
 /* eslint-disable linebreak-style */
 module.exports = {
-  name: "start",
-  description: "start command",
-  execute(message, args) {
-    const { games } = args;
-    if (games.has(message.channel.id)) {
-      message.channel.send("Game is already in progress in this channel!");
-      return;
-    }
-    games.set(message.channel.id, true);
-    message.channel.send("Welcome to planning poker");
-    message.channel.send("Please start your first round with !play [QUESTION]");
-    message.channel.send(
-      "For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
-    );
-    message.channel.send("You can stop playing by typing !end");
+	name: "start",
+	description: "start command",
+	execute(message, args) {
+		const { games } = args;
+		if (games.has(message.channel.id)) {
+			message.channel.send("Game is already in progress in this channel!");
+			return;
+		}
+		games.set(message.channel.id, true);
+    message.channel.send(ascii1);
+    message.channel.send(ascii2);
+		message.channel.send("Welcome to planning poker");
+		message.channel.send("Please start your first round with !play [QUESTION]");
+		message.channel.send(
+			"For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
+		);
+		message.channel.send("You can stop playing by typing !end");
 
-    return;
-  },
+		return;
+	},
 };
