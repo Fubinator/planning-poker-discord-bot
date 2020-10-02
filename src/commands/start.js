ddc8269b43ff1cbed2d6100c4fc5effc71bbedda
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2020-10-02 14:58:09 +0530

    fixes formatting issues

--- src/commands/start.js
+++ src/commands/start.js
@@ -17,24 +17,21 @@ _____     _              _____     _
 \`\`\`
 `;
 module.exports = {
-	name: "start",
-	description: "start command",
-	execute(message, args) {
-		const { games } = args;
-		if (games.has(message.channel.id)) {
-			message.channel.send("Game is already in progress in this channel!");
-			return;
-		}
-		games.set(message.channel.id, true);
-		message.channel.send(ascii1);
-		message.channel.send(ascii2);
-		message.channel.send("Welcome to planning poker");
-		message.channel.send("Please start your first round with !play [QUESTION]");
-		message.channel.send(
-			"For each round you've got 30 seconds to write your guessed number of story points via dm to the bot"
-		);
-		message.channel.send("You can stop playing by typing !end");
-
-		return;
-	},
+  name: "start",
+  description: "start command",
+  execute(message, args) {
+    const { games } = args;
+    if (games.has(message.channel.id)) {
+      message.channel.send("Game is already in progress in this channel!");
+      return;
+    }
+    games.set(message.channel.id, true);
+    message.channel.send(ascii1);
+    message.channel.send(ascii2);
+    message.channel.send("Welcome to planning poker");
+    message.channel.send("Please start your first round with !play [QUESTION]");
+    message.channel.send("For each round you've got 30 seconds to write your guessed number of story points via dm to the bot");
+    message.channel.send("You can stop playing by typing !end");
+    return;
+    },
 };
